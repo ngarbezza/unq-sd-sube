83f6349cc621c1d79a0dcc51c0aa6e98d181e5ad
Rocio Amaya y Nahuel Garbezza <amaya-garbezza@example.com>
2018-07-05 06:34:49 -0300

    configurar formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,11 @@
 # Used by "mix format"
 [
-  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: [
+    "mix.exs",
+    "{config,lib,test}/**/*.{ex,exs}"
+  ],
+  locals_without_parens: [
+    expect: 2
+  ],
+  line_length: 120
 ]
